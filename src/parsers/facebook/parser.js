70fa9c801d1e31e4d34f496316dc830ab7f5ba3f
GitStearis <egor.puisha@gmail.com>
2020-03-03 23:25:55 +0300

    Use notification route before logout, implement link beautifying

--- src/parsers/facebook/parser.js
+++ src/parsers/facebook/parser.js
@@ -8,6 +8,9 @@ const {
   extractSingleContent,
   extractSingleImage
 } = require("../../utils/puppeteer");
+const {
+  beautifyEventLink
+} = require("../../utils/text");
 
 const desktopUrl = "https://www.facebook.com";
 const urlParameters = "?suggestion_token=%7B%22time%22%3A%22tomorrow%22%2C%22city%22%3A%22107677462599905%22%7D&acontext=%7B%22source%22%3A2%2C%22source_dashboard_filter%22%3A%22discovery%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22dashboard%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22dashboard_home_discovery_filter%5C%22%7D%2C%7B%5C%22surface%5C%22%3A%5C%22discover_filter_list%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22surface%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22dashboard_filter%5C%22%3A%5C%22discovery%5C%22%7D%7D]%22%2C%22has_source%22%3Atrue%7D";
@@ -54,7 +57,7 @@ const extractLocation = async page => {
       const linkElement = locationContainer.querySelector("a");
 
       place = linkElement.innerText;
-      link = linkElement.href;
+      link = beautifyEventLink(linkElement.href);
       address = addressElement.innerText;
     } else {
       place = "";
