7d3c624c1b91a3272e9272ff8756cd20347c76d1
GitStearis <egor.puisha@gmail.com>
2020-05-10 00:02:41 +0300

    Make tutby parser to run first

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ require("./src/mongoose").connect();
 
 const logger = require("./src/utils/logger");
 
-const facebook = require("./src/parsers/facebook");
 const tutby = require("./src/parsers/tutby");
+const facebook = require("./src/parsers/facebook");
 
 (async () => {
     try {
         const parsers = [
-            facebook,
-            tutby
+            tutby,
+            facebook
         ];
 
         for (const parser of parsers) {
